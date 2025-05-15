81607f87212e46f8ab10c630263e4d1ccd625dd5
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2025-05-15 01:21:41 -0400

    fix: wrap layout in suspense to avoid build failures due to useSearchParams

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+        <Suspense>{children}</Suspense>
         <Toaster />
       </body>
     </html>
